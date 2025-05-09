b5d9bbff194c3a584cbc85e92c1aaf96d95bd340
Thizara2001 <thisarakavishani3@gmail.com>
2025-05-09 17:28:36 +0530

    Add support for new room and furniture types Extend designService to handle additional room types (office, study, penthouse, party) and furniture types (bookshelf, chair, table, lamp, rug).

--- src/services/designService.ts
+++ src/services/designService.ts
@@ -3,6 +3,18 @@ import { Furniture } from "../models/furniture/furniture";
 import { LivingRoom, LivingRoomData } from "../models/rooms/livingRoom";
 import { Sofa, SofaData } from "../models/furniture/sofa";
 import { DesignCreate, DesignUpdate, DesignData } from "./api";
+import { OfficeRoom, OfficeRoomData } from "../models/rooms/officeRoom.ts";
+import { StudyRoom, StudyRoomData } from "../models/rooms/studyRoom.ts";
+import {
+  PentHouseRoom,
+  PentHouseRoomData,
+} from "../models/rooms/pentHouseRoom.ts";
+import { PartyRoom, TShapedRoomData } from "../models/rooms/partyRoom.ts";
+import { Chair, ChairData } from "../models/furniture/chair.ts";
+import { Bookshelf, BookshelfData } from "../models/furniture/bookshelf.ts";
+import { Table, TableData } from "../models/furniture/table.ts";
+import { Lamp, LampData } from "../models/furniture/lamp.ts";
+import { Rug, RugData } from "../models/furniture/rug.ts";
 
 /**
  * Converts a room and furniture to a design for the API
@@ -72,6 +84,18 @@ export const createModelsFromDesign = (
     case "livingRoom":
       room = LivingRoom.fromData(designData.room.data as LivingRoomData);
       break;
+    case "officeRoom":
+      room = OfficeRoom.fromData(designData.room.data as OfficeRoomData);
+      break;
+    case "studyRoom":
+      room = StudyRoom.fromData(designData.room.data as StudyRoomData);
+      break;
+    case "pentHouseRoom":
+      room = PentHouseRoom.fromData(designData.room.data as PentHouseRoomData);
+      break;
+    case "partyRoom":
+      room = PartyRoom.fromData(designData.room.data as TShapedRoomData);
+      break;
     default:
       throw new Error(`Unknown room type: ${designData.room.type}`);
   }
@@ -81,6 +105,23 @@ export const createModelsFromDesign = (
     switch (designData.furniture.type) {
       case "sofa":
         furniture = Sofa.fromData(designData.furniture.data as SofaData);
+        break;
+      case "bookshelf":
+        furniture = Bookshelf.fromData(
+          designData.furniture.data as BookshelfData,
+        );
+        break;
+      case "chair":
+        furniture = Chair.fromData(designData.furniture.data as ChairData);
+        break;
+      case "table":
+        furniture = Table.fromData(designData.furniture.data as TableData);
+        break;
+      case "lamp":
+        furniture = Lamp.fromData(designData.furniture.data as LampData);
+        break;
+      case "rug":
+        furniture = Rug.fromData(designData.furniture.data as RugData);
         break;
       default:
         throw new Error(`Unknown furniture type: ${designData.furniture.type}`);
